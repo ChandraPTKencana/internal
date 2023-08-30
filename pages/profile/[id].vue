03f5f604daa7fd59eebcc08f04c595feb4d42543
ChandraLim92 <seven7planet@yahoo.com>
2023-08-30 13:05:25 +0700

    init

---
+++ pages/profile/[id].vue
@@ -0,0 +1,11 @@
+<script setup>
+// const route = useRoute();
+// console.log(route.params.id)
+
+const nuxt = useNuxtApp();
+</script>
+<template>
+    <div>
+        event id {{ $forceUpdate.params.id }}
+    </div>
+</template>
