e389cfaccfdf99bc915dd77e469c8c69124c955b
chandra <chandra>
2024-01-24 08:16:24 +0700

    favicon need to add stok in production

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,7 +21,7 @@ export default defineNuxtConfig({
         {
           rel: "icon",
           type: "image/png",
-          href: "/favicon.png"
+          href: "/stok/favicon.png"
         }
       ]
     }
